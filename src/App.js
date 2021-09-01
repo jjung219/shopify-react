fef6bbe6bc43cb5e71c3be982a217ecda35a727c
Jennifer Jung <jenjung2194@gmail.com>
2021-08-31 22:58:45 -0400

    set up react app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
+
 import './App.css';
 
 function App() {
+  const [orders, setOrders] = useState([]);
+
+  useEffect(() => {
+
+  });
+
+  const ordersList = this.state.orders.map(order => {
+    return (
+      <span key={order.id}>{order.name}</span>
+    )
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>
+        Shopify Reports Page
+      </h1>
+
+      <h3>Recent Orders</h3>
+      <div className="orders">
+
+      </div>
+
     </div>
   );
 }
